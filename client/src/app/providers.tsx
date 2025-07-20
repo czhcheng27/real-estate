acfd4566a3f0a7867a518eefb76185db643e91e9
czhcheng27 <czhcheng27@gmail.com>
2025-07-20 15:13:58 -0400

    signin/signup cognito

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,9 +1,17 @@
 "use client";
 import React, { ReactNode } from "react";
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
+import Auth from "./(auth)/authProvider";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
 };
 
 export default Providers;
