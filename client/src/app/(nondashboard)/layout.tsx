6b4f534ba1dd753c633d6e923863e8af83987d7a
czhcheng27 <czhcheng27@gmail.com>
2025-07-27 20:14:07 -0400

    layout

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,8 +1,33 @@
-import React, { ReactNode } from "react";
+"use client";
+
+import React, { useEffect, useState, ReactNode } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useGetAuthUserQuery } from "@/state/api";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
 import Navbar from "@/components/Navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
+
   return (
     <div>
       <Navbar />
